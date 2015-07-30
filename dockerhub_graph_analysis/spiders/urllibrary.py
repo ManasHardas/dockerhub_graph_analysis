31c8799404b1ac075ba45e45918125594a457873
Manas Hardas <manas@manashardas.com>
2015-07-30 16:46:12 -0700

    pagination

--- dockerhub_graph_analysis/spiders/urllibrary.py
+++ dockerhub_graph_analysis/spiders/urllibrary.py
@@ -5,8 +5,10 @@ import json
 def github_urls():
 	baseurl = 'https://api.github.com/search/repositories'
 	queryparams = '?q=docker%20in:name,description,readme'
+	pagination = '&per_page=50'
 
 	r = requests.get(baseurl + queryparams)
+	r = requests.get(baseurl + queryparams + pagination, auth=(user, password))
 	if(r.ok):
 		repoItems = json.loads(r.text or r.content)
 		urllist = map(html_url_getter, repoItems['items'])
