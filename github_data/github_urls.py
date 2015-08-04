b2a5f4f4098024db896a3745c81402e03f296867
Manas Hardas <manas@manashardas.com>
2015-08-04 09:19:59 -0700

    Generate single pandas data frame with all repo urls from cvs files

--- github_data/github_urls.py
+++ github_data/github_urls.py
@@ -1,22 +1,31 @@
 import pandas
+from pandas.core.series import Series
+from pandas.core.frame import DataFrame
 
+data_files_path = 'github_data/data/'
 
-github_repos_with_kw_docker =
-
-df_github_repos_with_kw_docker_2011_to_2014 = pandas.read_csv(
+repos_with_kw_docker_2011_to_2014_filepath = data_files_path + \
     'repos_with_docker_2011_to_2014.csv'
-)['repository_url']
+
+repos_with_kw_docker_2015_filepath = data_files_path + \
+    'repos_with_docker_2015.csv'
 
 
-df_repos_2015 = pandas.read_csv('repos_with_docker_2015.csv')['repo_url']
-df_github_repos_with_kw_docker_2015 = map(apiurl_to_repourl, df_repos_2015)
+df_github_repos_with_kw_docker_2011_to_2014 = DataFrame(pandas.read_csv(
+    repos_with_kw_docker_2011_to_2014_filepath
+)['repository_url'])
 
 
 def apiurl_to_repourl(apiurl):
-    repo_url = apiurl.replace('api.', '').replace('repos/', '')
+    return apiurl.replace('api.', '').replace('repos/', '')
 
+df_repos_2015 = pandas.read_csv(repos_with_kw_docker_2015_filepath)['repo_url']
+df_github_repos_with_kw_docker_2015 = DataFrame({
+        'repository_url': map(apiurl_to_repourl, df_repos_2015)
+    })
 
 df_repo_urls_with_kw_docker_2011_to_2015 = \
     df_github_repos_with_kw_docker_2011_to_2014.append(
-        df_github_repos_with_kw_docker_2015
+        df_github_repos_with_kw_docker_2015,
+        ignore_index=True
     )
